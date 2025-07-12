f569af34c69a69e7c14167020952409e03ab9dd5
Areez Khan <areezk79@gmail.com>
2025-07-13 01:09:46 +0500

    Quiz Game Resolution Corrected

--- Assets/QuizGame/QuizScripts/Mono/GameManager.cs
+++ Assets/QuizGame/QuizScripts/Mono/GameManager.cs
@@ -147,7 +147,7 @@ public class GameManager : MonoBehaviour {
         bool isCorrect = CheckAnswers();
         FinishedQuestions.Add(currentQuestion);
 
-        UpdateScore((isCorrect) ? Questions[currentQuestion].AddScore : -Questions[currentQuestion].AddScore);
+        UpdateScore((isCorrect) ? Questions[currentQuestion].AddScore : 0);
 
         if (IsFinished)
         {
